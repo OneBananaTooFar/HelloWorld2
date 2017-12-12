f9d2a0ff1e2b37deea5946b5c86dcec3cd651e3c
OneBananaTooFar <juliosd1@learn.cssd.ab.ca>
2017-12-12 13:21:59 -0700

    UVS

---
+++ HelloWorldiOS/detailsViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace HelloWorldiOS
+{
+    public partial class detailsViewController : UIViewController
+    {
+        public detailsViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
