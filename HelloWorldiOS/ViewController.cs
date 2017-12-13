c14537036f45fcb4971f9e54c5f377dc72102f9f
OneBananaTooFar <juliosd1@learn.cssd.ab.ca>
2017-12-13 13:34:52 -0700

    UVS

--- HelloWorldiOS/ViewController.cs
+++ HelloWorldiOS/ViewController.cs
@@ -6,6 +6,13 @@ namespace HelloWorldiOS
 {
     public partial class ViewController : UIViewController
     {
+        float num1;
+        float num2;
+        float result;
+
+        int option;
+
+
         protected ViewController(IntPtr handle) : base(handle)
         {
             // Note: this .ctor should not contain any initialization logic.
@@ -30,11 +37,11 @@ namespace HelloWorldiOS
 
 		void ButtonFill_TouchUpInside(object sender, EventArgs e)
 		{
-			float num1 = float.Parse(txt1stNum.Text);
-			float num2 = float.Parse(txt2ndNum.Text);
+			 num1 = float.Parse(txt1stNum.Text);
+			 num2 = float.Parse(txt2ndNum.Text);
 
-
-			float result = num1 + num2;
+            option = 1;
+			 result = num1 + num2;
 
             labelAnswer.Text = result.ToString();
 		}
@@ -42,33 +49,33 @@ namespace HelloWorldiOS
 
         void ButtonSubtract_TouchUpInside(object sender, EventArgs e)
         {
-			float num1 = float.Parse(txt1stNum.Text);
-			float num2 = float.Parse(txt2ndNum.Text);
+			 num1 = float.Parse(txt1stNum.Text);
+			 num2 = float.Parse(txt2ndNum.Text);
 
-
-			float result = num1 - num2;
+            option = 2;
+			 result = num1 - num2;
 
 			labelAnswer.Text = result.ToString();
         }
 
         void ButtonMultiply_TouchUpInside(object sender, EventArgs e)
         {
-			float num1 = float.Parse(txt1stNum.Text);
-			float num2 = float.Parse(txt2ndNum.Text);
+			 num1 = float.Parse(txt1stNum.Text);
+			 num2 = float.Parse(txt2ndNum.Text);
+            option = 3;
 
-
-			float result = num1 * num2;
+			 result = num1 * num2;
 
 			labelAnswer.Text = result.ToString();
         }
 
         void ButtonDivide_TouchUpInside(object sender, EventArgs e)
         {
-			float num1 = float.Parse(txt1stNum.Text);
-			float num2 = float.Parse(txt2ndNum.Text);
+			 num1 = float.Parse(txt1stNum.Text);
+			 num2 = float.Parse(txt2ndNum.Text);
 
-
-			float result = num1 / num2;
+            option = 4;
+			 result = num1 / num2;
 
 			labelAnswer.Text = result.ToString();
         }
@@ -77,6 +84,20 @@ namespace HelloWorldiOS
         {
             base.DidReceiveMemoryWarning();
             // Release any cached data, images, etc that aren't in use.
+        }
+
+        public override void PrepareForSegue(UIStoryboardSegue segue, Foundation.NSObject sender)
+        {
+            base.PrepareForSegue(segue, sender);
+
+            var detailsViewController = segue.DestinationViewController as detailsViewController;
+
+            detailsViewController.num1 = this.num1;
+            detailsViewController.num2 = this.num2;
+            detailsViewController.option = this.option;
+            detailsViewController.result = this.result;
+                
+        
         }
     }
 }
--- HelloWorldiOS/detailsViewController.cs
+++ HelloWorldiOS/detailsViewController.cs
@@ -6,8 +6,41 @@ namespace HelloWorldiOS
 {
     public partial class detailsViewController : UIViewController
     {
-        public detailsViewController (IntPtr handle) : base (handle)
+        public float num1 = 10f;
+        public float num2 = 20;
+
+        public int option = 1;
+        public float result = 30f;
+
+        public detailsViewController(IntPtr handle) : base(handle)
         {
+
+        }
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            switch(option){
+                case 1: 
+                    theRealLabelDetails.Text = num1.ToString() + " + " + num2.ToString() + " = " + result.ToString();
+                    break;
+                case 2:
+                    theRealLabelDetails.Text = num1.ToString() + " - " + num2.ToString() + " = " + result.ToString();
+                    break;
+                case 3:
+                    theRealLabelDetails.Text = num1.ToString() + " * " + num2.ToString() + " = " + result.ToString();
+                    break;
+                case 4:
+                    theRealLabelDetails.Text = num1.ToString() + " / " + num2.ToString() + " = " + result.ToString();
+                    break;
+                    
+            }
+        
         }
     }
+
 }
+
+
+
+
